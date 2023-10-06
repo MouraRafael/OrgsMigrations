d32478a7f48ae7acea2bcfe7e92d0a310c99fd77
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-06 16:31:28 -0300

    4-1-06     Faça como eu fiz: configurando o usuário com o Room

--- app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
+++ app/src/main/java/br/com/alura/orgs/database/AppDatabase.kt
@@ -7,17 +7,22 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import br.com.alura.orgs.database.converter.Converters
 import br.com.alura.orgs.database.dao.ProdutoDao
+import br.com.alura.orgs.database.dao.UsuarioDao
 import br.com.alura.orgs.model.Produto
+import br.com.alura.orgs.model.Usuario
 
 @Database(
     entities = [
-        Produto::class
+        Produto::class,
+        Usuario::class
     ],
     version = 1,
     exportSchema = true
 )
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun usuarioDao():UsuarioDao
 
     abstract fun produtoDao(): ProdutoDao
 
--- app/src/main/java/br/com/alura/orgs/ui/activity/FormularioCadastroUsuarioActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioCadastroUsuarioActivity.kt
@@ -2,11 +2,19 @@ package br.com.alura.orgs.ui.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import br.com.alura.orgs.database.AppDatabase
 import br.com.alura.orgs.databinding.ActivityFormularioCadastroUsuarioBinding
 import br.com.alura.orgs.model.Usuario
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class FormularioCadastroUsuarioActivity : AppCompatActivity() {
+    private val dao by lazy {
+        AppDatabase.instancia(this).usuarioDao()
+    }
 
     private val binding by lazy {
         ActivityFormularioCadastroUsuarioBinding.inflate(layoutInflater)
@@ -21,6 +29,16 @@ class FormularioCadastroUsuarioActivity : AppCompatActivity() {
     private fun configuraBotaoCadastrar() {
         binding.activityFormularioCadastroBotaoCadastrar.setOnClickListener {
             val novoUsuario = criaUsuario()
+            lifecycleScope.launch(){
+                try {
+                    dao.salva(novoUsuario)
+                    finish()
+                }catch (e:Exception){
+                    Log.e("CadastroUsuarioo", "Configurando cadastrar")
+                    Log.e("CadastroUsuarioo", "${e.printStackTrace()}")
+                    Toast.makeText(this@FormularioCadastroUsuarioActivity,"Falha ao cadastrar usuario",Toast.LENGTH_SHORT).show()
+                }
+            }
             Log.i("CadastroUsuario", "onCreate: $novoUsuario")
             finish()
         }
